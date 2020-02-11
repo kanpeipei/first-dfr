e93bba1cc6fef8e5018d2a3ba50b8951136718b2
kanpeipei <satosjhkkmp@gmail.com>
2020-02-11 12:00:46 +0900

    collectstaticを実行

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
